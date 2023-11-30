065915c19bb7ff2cc07d3f3f9e377657974fc241
RicardoEspiritu <95498222+Richard5102@users.noreply.github.com>
2023-11-30 16:09:55 -0600

    Add find all and get by ID methods

---
+++ src/main/java/org/example/Main/Main.java
@@ -0,0 +1,20 @@
+package org.example.Main;
+
+import org.example.model.Employee;
+import org.example.repository.EmployeeRepository;
+import org.example.repository.Repository;
+import org.example.util.DatabaseConnection;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+
+        try(Connection myConn = DatabaseConnection.getInstance())  {
+            Repository<Employee> repository = new EmployeeRepository();
+            repository.findAll().forEach(System.out::println);
+        }
+    }
+}
---
+++ src/main/java/org/example/repository/Repository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Repository<T> {
+
+    List<T> findAll() throws SQLException;
+    T getByID(Integer id) throws SQLException;
+
+    void save(T t);
+
+    void delete(Integer id);
+}
